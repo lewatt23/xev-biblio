062bccddeaa4bc7bf921e8a57e730bf7fc791372
lewatt23 <lewatt23@gmail.com>
2024-06-10 17:23:18 +0100

    chore: added tailwind and design login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
